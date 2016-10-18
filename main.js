67cc96eb07019f664df10baf274c3d3fbaba4a95
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-10-18 00:02:25 -0400

    EOD monday

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
+$("xxx").on("click"), function(e) {
+    var xxx = $("xxx").val();
+
+}
+
+
+
 $("button").on("click", function(e) {
-var search = $("#search").val();
-writeData(search);
+            var search = $("#search").val();
 
+            $.ajax({
+                url: "http://api.soundcloud.com/tracks/?client_id=03e4633e2d85874a921380e47cac705d&q=${search}",
+                success: function(result) {
+                    console.log(result);
+                    // Assign the data variable to an array that we will loop over.
+                    // Clear out any previous data.
+                    $("#search").html("");
+                    // Put in the results
 
+                    // Loop over our array to get access to each day.
+                    result.forEach(function(track) {
+                        // Get the icon url for this day
+                        var iconUrl = `xxx`;
 
-$.ajax({
-data: { search: search },
-url: "https://api.soundcloud.com 03e4633e2d85874a921380e47cac705d",
-success: function(result) {
-// Assign the data variable to an array that we will loop over.
-var data = result.list;
-// Clear out any previous data.
-$("#search").html("");
-// Put in the results
-$("#searchResultsGoHere").text(result.city.name);
+                        // Put the search results info into our page.
+                        $("#searchResultsGoHere").append(`
+		<div class="music col-md-4">
+		<h3></h3>
+		<img src="${track.artwork_url}">
+		<h3>Song Title:<br>${track.title}</h3>
+		</div>
+		`)
+                    })
+                }
+            });
+        });
 
-// Loop over our array to get access to each day.
-data.forEach(function(search) {
-// Get the icon url for this day
-var iconUrl = `xxx`;
-
-// Put the search results info into our page.
-$("#searchResultsGoHere").append(`
-<div class="music col-md-3">
-<h3>${ }</h3>
-<h3>Song Title:${day.temp.max}</h3>
-<h3>Artist:${day.temp.min}</h3>
-<img src="${iconUrl}">
-</div>
-`)
-})
-}
-});
-}
